0b4b02ae17d2afe8464fe82c7449229eb8df64e5
Money <2932978634@qq.com>
2024-03-27 16:15:40 +0800

    最后一次提交

--- springboot/src/main/java/com/example/demo/pojo/Login.sql
+++
@@ -1,7 +0,0 @@
--- auto Generated on 2023-06-27
--- DROP TABLE IF EXISTS user_login;
-CREATE TABLE user_db(
-    id INT NOT NULL AUTO_INCREMENT COMMENT '用户id',
-	email VARCHAR (50) NOT NULL COMMENT '注册邮箱',
-	PRIMARY KEY (id)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT '在线用户表';
